e5e6b71c54b1e3cd8b6873df7eae51fa2ed84cd2
Jiaoyan Chen <chen00217@gmail.com>
2013-05-26 11:00:27 +0800

    rdf code

--- PM_Data/src/rdf/pm/spatial/bigdata/RDFCreate.java
+++ PM_Data/src/rdf/pm/spatial/bigdata/RDFCreate.java
@@ -1,5 +1,9 @@
 package rdf.pm.spatial.bigdata;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
@@ -8,8 +12,42 @@ public class RDFCreate {
 	public static String uri="http://www.bigspatialdata.org/pm";
 	public static Resource measurements(String id){
 		Model m = ModelFactory.createDefaultModel();
+		String [] values = getStationRecord(id);
 		Resource measurements = m.createResource(uri+"#" + id);
 		
 		return measurements;
+	}
+	public static String [] getStationRecord(String id){
+		String sql = "select * from Station_Data where ID=" + id;
+		String [] values = null;
+		try {
+			java.sql.Connection conn = DriverManager.getConnection(
+			    		"jdbc:mysql://10.214.0.147/pm0?useUnicode=true&characterEncoding=UTF-8", "pm", "ccntgrid");
+			java.sql.Statement stmt = conn.createStatement();
+			ResultSet result = stmt.executeQuery(sql);
+			if(result.next()){
+				values = new String[13];
+				values[0] = result.getString("cityNameCh");
+				values[1] = result.getString("stationNameCh");
+				values[2] = result.getString("stationCode");
+				values[3] = result.getString("AQI");
+				values[4] = result.getString("Quality");
+				values[5] = result.getString("CO");
+				values[6] = result.getString("SO2");
+				values[7] = result.getString("NO2");
+				values[8] = result.getString("O3");
+				values[9] = result.getString("PM10");
+				values[10] = result.getString("PM2p5");
+				values[11] = result.getString("primary_pollutant");
+				values[12] = result.getString("time_point");
+			}
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Unable to read data from mysql.");
+		}
+		return values;
 	}
 }
